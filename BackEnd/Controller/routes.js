e8106a5c7e8f7802cb355935899d1a285a67ef03
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-07 22:38:39 +0800

    Created a middleware for checking token and verifying if it valid

--- BackEnd/Controller/routes.js
+++ BackEnd/Controller/routes.js
@@ -1,7 +1,7 @@
 import { pool } from '/ExpressJS-ToDoList/BackEnd/Database/database.js';
 import bcrypt from 'bcrypt';
 import express from 'express';
-import { createToken } from '../Middlewares/authentication';
+import { createToken, authenticated } from '/ExpressJS-ToDoList/BackEnd/Middlewares/authentication.js';
 
 export const router = express();
 
@@ -23,21 +23,36 @@ router.post('/auth/signup', async (req, res) => {
 
 router.post('/auth/login', async (req, res) => {
     const {username, password} = req.body;
+
+    //Query in the postgres to get the users
     const user = await pool.query(
         `SELECT users.username, users.password FROM users WHERE username = $1`,
         [username]
     );
     const foundUser = user.rows[0];
     console.log(foundUser.username);
+
+    //Check if the query has found a user
     if(user.rowCount === 0){
         console.log('The username is incorrect!');
         return res.status(404).send('Incorrect username!');
     }
+
+    /*
+      A boolean checking if the password and the hash password
+      is equal
+    */
     const unhashPass = await bcrypt.compare(password, foundUser.password);
-    if(!unhashPass){
-        return res.status(404).send('Incorrect password!');
-    } 
-    createToken()
+
+    if(!unhashPass) return res.status(404).send('Incorrect password!');
+
+    const token = createToken(foundUser.username, password);
+
+    return res.status(200).json({ token });
+});
+
+router.get('/protected/tasks', authenticated, async (req, res) => {
+    res.status(200).send('Welcome');
 });
 
 router.post('/items/add', async (req, res) => {
--- BackEnd/Middlewares/authentication.js
+++ BackEnd/Middlewares/authentication.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const SECRET_KEY = 'cfc441f8ca1d0058aab81d8d6eaeb975267b06d6683a5e1026795cbf37fa6a7f';
 
+//Create a token for authentication
 export const createToken = (userUsername, userPassword) => {
     const payload = {username: userUsername, password: userPassword};
 
@@ -10,5 +11,16 @@ export const createToken = (userUsername, userPassword) => {
     return token;
 };
 
+export const authenticated = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    const token = bearerHeader && bearerHeader.split(' ')[1];
 
+    if(!token) return res.status(403).json('Not valid!');
+
+    jwt.verify(token, SECRET_KEY, (err, decode) => {
+        if(err) return res.status(403).json('Not valid!');
+        req.user = decode;
+        next();
+    });
+};
 
