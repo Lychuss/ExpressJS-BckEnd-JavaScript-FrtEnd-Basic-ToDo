047dfca6443704c8c48d45b503505082853a342d
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-09-15 21:39:13 +0800

    fix the bug in the frontend but was not able to do it by myself, ill still try to learn it, my next is to display task automatically if has values

--- FrontEnd/Service/home.js
+++ FrontEnd/Service/home.js
@@ -5,10 +5,8 @@ const tasks = document.getElementById('tasks');
 const task = document.getElementById('task');
 const allTask = document.getElementById('allTask');
 
-const userTasks = JSON.parse(localStorage.getItem('tasks')) || []; 
-const userId = JSON.parse(localStorage.getItem('user'));
-
-window.addEventListener('DOMContentLoaded', displayTask);
+let userTasks = JSON.parse(localStorage.getItem('tasks')) || []; 
+let userId = JSON.parse(localStorage.getItem('user'));
 
 add.addEventListener('click', async () => {
 
@@ -39,19 +37,29 @@ function displayTask(){
         h1.className = 'taskClass';
 
         const createImg = document.createElement('img');
-        createImg.onclick = async () => {
-            if(createDiv.id === userTasks[i].id){
-                userTasks.splice[1, i];
-                localStorage.setItem('tasks', JSON.stringify(userTasks));
-            }
-            createDiv.remove();
-            console.log(createDiv.id);
-            websitelogics.removeTask(userId.userId, createDiv.id, userId.token);
-        };
         
         createImg.className = 'remove';
         createImg.alt = 'remove';
         createImg.src = '/FrontEnd/images/remove.png';
+        createImg.onclick = function() {
+            const divId = this.closest('div').id;
+
+            const index = userTasks.findIndex(task => task.id === divId);
+
+            if(index !== -1){
+                userTasks.splice(index, 1);
+            }
+
+            localStorage.setItem('tasks', JSON.stringify(userTasks));
+
+            userTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+            
+            this.closest('div').remove();
+
+            console.log(userTasks);
+
+            websitelogics.removeTask(userId.userId, divId, userId.token);
+        };
 
         createDiv.append(h1, createImg);
 
@@ -59,6 +67,18 @@ function displayTask(){
 
         console.log('whatt');
     }
+}
+
+function removeTask(value){
+    const divId = value.closest('div').id;
+        userTasks.splice[0, divId];
+
+        localStorage.setItem('tasks', JSON.stringify(userTasks));
+
+        value.closest('div').remove();
+
+        console.log(divId);
+        websitelogics.removeTask(userId.userId, divId, userId.token);
 }
 
 
